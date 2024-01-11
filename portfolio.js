0b5db593b60134a278dab14c1c89c3f654877599
MogiliVarshini <mogilivarshini@gmail.com>
2024-01-11 14:23:11 +0530

    add

--- portfolio.js
+++ portfolio.js
@@ -36,3 +36,36 @@ $('.multi-item-carousel .item').each(function(){
     $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
   }
 });
+
+/*Change Mode*/
+
+function changeMode()
+{
+    let element = document.body;
+      document.body.classList.toggle("dark");
+
+}
+
+'use strict';
+
+function progressBarAndCountNumber () {
+    const progress = document.querySelectorAll('.progressive');
+    let count = 0;
+    // You must put the maximum number in the MAX variable.
+    let MAX = 99;
+
+    let run = setInterval(() => {
+        count++;
+        progress.forEach(element => {
+            if (count <= element.dataset.progressive) {
+                element.parentElement.style.background = `conic-gradient(#f9004d ${count}%, #212428 0)`;
+                element.firstElementChild.textContent = `${count}%`;
+            };
+            if (count == MAX) {
+                clearInterval(run);
+            };
+        });
+    }, 20);
+}
+
+progressBarAndCountNumber();
